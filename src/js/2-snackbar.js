1dc7deb8cbb205e3d2313834b987d4191e8c4d3c
OlhaKatrych <olgak198931@gmail.com>
2024-01-22 14:36:51 +0200

    create promise

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,43 @@
+import izitoast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
+const form = document.querySelector('.form');
+const inputDelay = form.elements.delay;
+let delay;
+inputDelay.addEventListener('input', handlerInput);
+form.addEventListener('submit', handlerSubmit);
+
+function handlerInput(e) {
+  delay = e.target.value;
+}
+
+function handlerSubmit(e) {
+  e.preventDefault();
+  const elements = {
+    fulfilled: form.elements.state.value,
+    rejected: form.elements.state.value,
+  };
+
+  const promise = new Promise((resolve, reject) => {
+    const timerID = setTimeout(() => {
+      if (elements.fulfilled === 'fulfilled') {
+        resolve(
+          izitoast.success({
+            message: `✅ Fulfilled promise in ${delay}ms`,
+            position: 'topRight',
+          })
+        );
+      } else if (elements.rejected === 'rejected') {
+        reject(
+          izitoast.error({
+            message: `❌ Rejected promise in ${delay}ms`,
+            position: 'topRight',
+          })
+        );
+      }
+    }, delay);
+  });
+
+  promise.then(value => value).catch(err => err);
+  form.reset();
+}
